131fade7a77a7a5711c751ced8e19da7c5162455
vlshevtsov <vladimir.shevtsov@aviasales.ru>
2025-02-27 15:04:28 +0400

    add infinite download of data

--- webapp/src/pages/ideas/AllIdeasPage/index.tsx
+++ webapp/src/pages/ideas/AllIdeasPage/index.tsx
@@ -3,35 +3,55 @@ import { getViewIdeaRoute } from '../../../lib/routes'
 import { trpc } from '../../../lib/trpc'
 import css from './index.module.scss'
 import { Segment } from '../../../components/Segment'
+import { Alert } from '../../../components/Alert'
 
 export const AllIdeasPage = () => {
-  const { data, error, isLoading, isFetching, isError } = trpc.getIdeas.useQuery()
-  
-  if (isLoading) {
-    return <span>Loading...</span>
-  }
+  const { data, error, isLoading, isFetching, isError, hasNextPage, fetchNextPage, isFetchingNextPage, isRefetching } = trpc.getIdeas.useInfiniteQuery({
+    limit: 2,
+  }, {
+    getNextPageParam: (lastPage) => {
+      return lastPage.nextCursor
+    }
+  })
 
-  if (isError) {
-    return <span>Error: {error.message}</span>
-  }
 
   return (
     <Segment title="All Ideas">
-      <div className={css.ideas}>
-        {data.ideas.map((idea) => (
-          <div className={css.idea} key={idea.nick}>
-            <Segment 
-              size={2} 
-              title={
-                <Link className={css.ideaLink} to={getViewIdeaRoute({ ideaNick: idea.nick })}>
-                  {idea.name}
-                </Link>
-              }
-              description={idea.description}
-            />
+      {isLoading || isRefetching ? (
+        <div>Loading...</div>
+      ) : isError ? (
+        <Alert color='red'>{error.message}</Alert>
+      ) : (
+        <div className={css.ideas}>
+          {data.pages.flatMap((page) => page.ideas)
+          .map((idea) => (
+            <div className={css.idea} key={idea.nick}>
+              <Segment 
+                size={2} 
+                title={
+                  <Link className={css.ideaLink} to={getViewIdeaRoute({ ideaNick: idea.nick })}>
+                    {idea.name}
+                  </Link>
+                }
+                description={idea.description}
+              />
+            </div>
+          ))}
+          <div className={css.more}>
+            {hasNextPage && !isFetchingNextPage && (
+              <button 
+                onClick={(e) => {
+                  e.preventDefault()
+                  void fetchNextPage()
+                }}
+              >
+                Load more
+              </button>
+            )}
+            {isFetchingNextPage && <span>Loading...</span>}
           </div>
-        ))}
-      </div>
+        </div>
+      )}
     </Segment>
   )
 }
