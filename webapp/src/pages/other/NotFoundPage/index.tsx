96010dcfd4e3444f41681923a439a04d9829ac5d
vlshevtsov <vladimir.shevtsov@aviasales.ru>
2025-03-04 14:13:16 +0400

    add 404 image

--- webapp/src/pages/other/NotFoundPage/index.tsx
+++ webapp/src/pages/other/NotFoundPage/index.tsx
@@ -1,4 +1,6 @@
 import { ErrorPageComponent } from '../../../components/ErrorPageComponent'
+import image404 from '../../../assets/images/404.png'
+import css from './index.module.scss'
 
 export const NotFoundPage = ({
   title = 'Not Found',
@@ -6,4 +8,6 @@ export const NotFoundPage = ({
 }: {
   title?: string
   message?: string
-}) => <ErrorPageComponent title={title} message={message} />
+}) => <ErrorPageComponent title={title} message={message}>
+  <img src={image404} className={css.image} alt="404" width="800" height="600" />
+</ErrorPageComponent>
