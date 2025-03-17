a44a0d1fe5d2dd19b270e9274f073142faf7fff1
vlshevtsov <vladimir.shevtsov@aviasales.ru>
2025-03-17 23:21:04 +0400

    add select query for most liked ideas

--- backend/src/lib/cron.ts
+++ backend/src/lib/cron.ts
@@ -1,11 +1,12 @@
 import { CronJob } from 'cron'
 import { type AppContext } from './ctx'
+import { notifyAboutMostLikedIdeas } from '../scripts/notifyAboutMostLikedIdeas'
 
 export const applyCron = (ctx: AppContext) => {
   new CronJob(
     '0 10 1 * *', // At 10:00 on day-of-month 1
     () => {
-      console.info('Hello!')
+      notifyAboutMostLikedIdeas(ctx).catch(console.error)
     },
     null, // onComplete
     true // start right now
---
+++ backend/src/scripts/notifyAboutMostLikedIdeas.ts
@@ -0,0 +1,28 @@
+import { type Idea } from '@prisma/client'
+import { type AppContext } from '../lib/ctx'
+
+export const notifyAboutMostLikedIdeas = async (ctx: AppContext) => {
+  const mostLikedIdeas = await ctx.prisma.$queryRaw<
+    Array<Pick<Idea, 'id' | 'nick' | 'name'> & { thisMonthLikesCount: number }>
+  >`
+    with "topIdeas" as (
+      select id,
+        nick,
+        name,
+        (
+          select count(*)::int
+          from "IdeaLike" il
+          where il."ideaId" = i.id
+            and il."createdAt" > now() - interval '1 month'
+            and i."blockedAt" is null
+        ) as "thisMonthLikesCount"
+      from "Idea" i
+      order by "thisMonthLikesCount" desc
+      limit 10
+    )
+    select *
+    from "topIdeas"
+    where "thisMonthLikesCount" > 0
+  `
+  console.info(mostLikedIdeas)
+}
